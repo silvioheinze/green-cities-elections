0397709843100d9984a9c00351fe4bbe65f3fadc
Silvio Heinze <mail@silvioheinze.eu>
2021-02-22 10:03:04 +0000

    add html table parser

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
 import requests
+from html_table_parser import HTMLTableParser
 import pandas as pd
 
 
-url = 'https://greeningthecities.wordpress.com/election-results/election-results-cities/''
+def url_get_contents(url): 
+	req = request.Request(url=url) 
+	f = request.urlopen(req) 
+  
+	return f.read() 
+  
+url = 'https://greeningthecities.wordpress.com/election-results/election-results-cities/'
+html = url_get_contents(url).decode('utf-8') 
+
+p = HTMLTableParser() 
+p.feed(html) 
+  
+print(p.tables[1]) 
+ 
+print(pd.DataFrame(p.tables[1]))
